b52d99271af20e680929f15b94419df195509a0a
lzn01 <408135242@qq.com>
2022-08-24 10:36:22 +0800

    全局样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Icon from '@/components/Icon';
+import '@/assets/styles/global.scss';
+import '@/assets/styles/reset.scss';
 
 ReactDOM.render(
-    <>
+    <div className={'player-container'}>
         <h1>Hello World</h1>
         <div style={{ background: '#818080' }}>
             <Icon name={'close-web-full-screen'}/>
         </div>
-    </>,
+    </div>,
     document.querySelector('#root'),
 );
