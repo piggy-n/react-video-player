4ed0cce1e59588234af858463368ef3ac3fda08c
lzn01 <408135242@qq.com>
2022-08-24 16:41:13 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,14 @@ module.exports = {
         '@typescript-eslint',
     ],
     'rules': {
-        'semi': ['error', 'always'],
-        'react/no-find-dom-node': ['off'],
-        '@typescript-eslint/no-explicit-any': ['off'],
-        '@typescript-eslint/no-empty-function': ['off'],
-        '@typescript-eslint/no-var-requires': ['off'],
+        'semi': ['error', 'always'], // 分号
+        'no-prototype-builtins': 'off', // 禁止使用 Object.prototypes 中的属性
+        'react/react-in-jsx-scope': 'off', // jsx 中的react
+        'react/no-find-dom-node': ['off'], // findDOMNode在函数组件中的使用
+        '@typescript-eslint/no-explicit-any': ['off'], // 不检查any类型
+        '@typescript-eslint/no-var-requires': ['off'], // 不检查require
+        '@typescript-eslint/consistent-type-imports': 'error', // 导入类型的规范
+        '@typescript-eslint/no-unused-vars': 'error', // 不检查未使用的变量
+        '@typescript-eslint/ban-ts-comment': ['off'],
     },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import '@/global.scss';
 import VideoPlayer from '@/core/VideoPlayer';
-import { CSSProperties } from 'react';
+import type { CSSProperties } from 'react';
 
 const styles: CSSProperties = {
     position: 'relative',
