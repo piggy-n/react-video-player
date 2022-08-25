0e07dcd183b777360d490eae634ec83ad899fe48
lzn01 <408135242@qq.com>
2022-08-25 15:01:34 +0800

    ControllerToolbar 按钮逻辑

--- src/core/Header/ControllerToolbar.tsx
+++ src/core/Header/ControllerToolbar.tsx
@@ -4,20 +4,89 @@ import type { ControllerToolbarProps } from '@/core/Header/type';
 import { classes } from '@/utils/methods/classes';
 import './styles/controllerToolbar.scss';
 import Icon from '@/components/Icon';
+import { useState } from 'react';
 
 const cn = 'Controller-Toolbar';
 
 const ControllerToolbar: FC<ControllerToolbarProps> = () => {
+    const [gridStatus, setGridStatus] = useState<Record<string, boolean>>({
+        singleGrid: true,
+        doubleGrid: false,
+        pip: false
+    });
+
+    const [panelStatus, setPanelStatus] = useState<Record<string, boolean>>({
+        screenshot: false,
+        control: false,
+        recording: false,
+    });
+
+    const gridStatusHandler = (key: string) => {
+        if (gridStatus[key]) return;
+
+        const newGridStatus = {
+            singleGrid: false,
+            doubleGrid: false,
+            pip: false,
+            [key]: !gridStatus[key],
+        };
+
+        setGridStatus(newGridStatus);
+    };
+
+    const panelStatusHandler = (key: string) => {
+        if (panelStatus[key]) return;
+
+        const newPanelStatus = {
+            screenshot: false,
+            control: false,
+            recording: false,
+            [key]: !panelStatus[key],
+        };
+
+        setPanelStatus(newPanelStatus);
+    };
+
     return (
         <div className={classes(cn, '')}>
-            <Icon name={'single-grid'} title={'单宫'}/>
-            <Icon name={'double-grid'} title={'双宫'}/>
-            <Icon name={'pip'} title={'画中画'}/>
-            <Icon name={'screenshot'} title={'查看截图'}/>
-            <Icon name={'controller'} title={'控制'}/>
-            <Icon name={'recording'} title={'查看录像'}/>
-            <Icon name={'fullscreen'} title={'全屏'}/>
-            <Icon name={'close'} title={'关闭'}/>
+            <Icon
+                name={gridStatus['singleGrid'] ? 'single-grid-active' : 'single-grid'}
+                title={'单宫'}
+                onClick={() => gridStatusHandler('singleGrid')}
+            />
+            <Icon
+                name={gridStatus['doubleGrid'] ? 'double-grid-active' : 'double-grid'}
+                title={'双宫'}
+                onClick={() => gridStatusHandler('doubleGrid')}
+            />
+            <Icon
+                name={gridStatus['pip'] ? 'pip-active' : 'pip'}
+                title={'画中画'}
+                onClick={() => gridStatusHandler('pip')}
+            />
+            <Icon
+                name={panelStatus['screenshot'] ? 'screenshot-active' : 'screenshot'}
+                title={'查看截图'}
+                onClick={() => panelStatusHandler('screenshot')}
+            />
+            <Icon
+                name={panelStatus['control'] ? 'control-active' : 'control'}
+                title={'控制'}
+                onClick={() => panelStatusHandler('control')}
+            />
+            <Icon
+                name={panelStatus['recording'] ? 'recording-active' : 'recording'}
+                title={'查看录像'}
+                onClick={() => panelStatusHandler('recording')}
+            />
+            <Icon
+                name={'fullscreen'}
+                title={'全屏'}
+            />
+            <Icon
+                name={'close'}
+                title={'关闭'}
+            />
         </div>
     );
 };
