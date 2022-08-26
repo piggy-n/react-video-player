c2db82a0700f5265d7580c9eb06ae8ab0b1dada7
lzn01 <408135242@qq.com>
2022-08-26 16:20:10 +0800

    调整目录结构

--- src/core/Player/index.ts
+++ src/core/Player/index.ts
@@ -1,3 +1,6 @@
 import Player from './Player';
+import PlayerController from './PlayerController';
+import ControlPanel from './ControlPanel';
 
+export { PlayerController, ControlPanel };
 export default Player;
