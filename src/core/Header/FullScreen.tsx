35922ab7e760b3ed93b2faf2498da7cc6239d785
lzn01 <408135242@qq.com>
2022-09-22 13:51:51 +0800

    画中画调整

--- src/core/Header/FullScreen.tsx
+++ src/core/Header/FullScreen.tsx
@@ -1,15 +1,61 @@
-import { isFullscreen } from 'screenfull';
+import screenfull, { isFullscreen } from 'screenfull';
 import Icon from '@/components/Icon';
 import * as React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { CtrPlayerContext } from '@/utils/hooks/useCtrPlayerContext';
 
 const FullScreen = () => {
-    const clickHandler = () => {
+    const {
+        ctrPlayerModel: {
+            playerWrapperEle,
+            doubleGrid
+        },
+        setCtrPlayerModelData
+    } = useContext(CtrPlayerContext);
 
+    const [isDoubleGrid, setIsDoubleGrid] = useState<boolean>(false);
+
+    const clickHandler = () => {
+        if (!playerWrapperEle || !setCtrPlayerModelData) return;
+
+        if (screenfull.isEnabled) {
+            screenfull.toggle(playerWrapperEle);
+        }
     };
+
+    useEffect(() => {
+        if (!setCtrPlayerModelData) return;
+
+        if (isFullscreen && doubleGrid) {
+            setCtrPlayerModelData({
+                type: 'doubleGrid',
+                payload: false
+            });
+
+            setCtrPlayerModelData({
+                type: 'pictureInPicture',
+                payload: true
+            });
+            setIsDoubleGrid(true);
+        }
+
+        if (!isFullscreen && isDoubleGrid) {
+            setCtrPlayerModelData({
+                type: 'doubleGrid',
+                payload: true
+            });
+
+            setCtrPlayerModelData({
+                type: 'pictureInPicture',
+                payload: false
+            });
+            setIsDoubleGrid(false);
+        }
+    }, [isFullscreen, isDoubleGrid, doubleGrid]);
 
     return (
         <Icon
-            name={isFullscreen ? 'close-web-fullscreen' : 'fullscreen'}
+            name={'fullscreen'}
             title={'全屏'}
             onClick={clickHandler}
         />
